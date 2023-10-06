2edd902521ae02d55dfabadcdac17a678e35e045
jennychiang <jennyc201911@gmail.com>
2023-10-07 00:43:30 +0800

    Update: Refine Todo controller - Integrate error handling for scenarios when records are not found - Improve variable naming for increased code readability

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -2,40 +2,51 @@ module Api
   module V1
     class TodosController < ApplicationController
       def index
-        @all_todos = Todo.all
-        render json: @all_todos
+        all_todos = Todo.all
+        render json: { status: "success", data: all_todos }, status: :ok
       end
 
       def create
         begin
-          @todo = TodoCreatorService.call(todo_params)
-          render json: { status: "success", data: @todo }, status: :created
+          todo = TodoCreatorService.call(todo_params)
+          render json: { status: "success", data: todo }, status: :created
         rescue StandardError => e
-          render json: { status: "error", errors: e.message }, status: :unprocessable_entity
+          render json: { status: "error", errors: e.message }, status: :internal_server_error
         end
       end
 
       def update
         begin
-          @updated_todo = TodoUpdateService.call(params[:id], todo_params)
-          render json: { status: "success", data: @updated_todo }, status: :ok
+          updated_todo = TodoUpdateService.call(params[:id], todo_params)
+          render json: { status: "success", data: updated_todo }, status: :ok
+        rescue ActiveRecord::RecordNotFound
+          render json: { status: "error", errors: "Todo not found" }, status: :not_found
         rescue StandardError => e
-          render json: { status: "error", errors: e.message }, status: :unprocessable_entity
+          render json: { status: "error", errors: e.message }, status: :internal_server_error
         end
       end
 
       def destroy
-        @todo = Todo.find(params[:id])
-        if @todo.destroy
-          render json: { status: "success", message: "Todo was successfully deleted." }, status: :ok
-        else
-          render json: { status: "error", message: "Failed to delete the todo." }, status: :unprocessable_entity
+        begin
+          destroy_todo = TodoDestroyService.call(params[:id])
+          render json: { status: "success", data: destroy_todo }, status: :ok
+        rescue ActiveRecord::RecordNotFound
+          render json: { status: "error", errors: "Todo not found" }, status: :not_found
+        rescue StandardError => e
+          render json: { status: "error", errors: e.message }, status: :internal_server_error
         end
       end
 
       def show
-        @todo = Todo.find(params[:id])
-        render json: @todo
+        begin
+          found_todo = TodoShowService.call(params[:id])
+          render json: { status: "success", data: found_todo }, status: :ok
+        rescue ActiveRecord::RecordNotFound
+          render json: { status: "error", errors: "Todo not found" }, status: :not_found
+        rescue StandardError => e
+          # This catches any other unexpected error.
+          render json: { status: "error", errors: e.message }, status: :internal_server_error
+        end
       end
 
       private
