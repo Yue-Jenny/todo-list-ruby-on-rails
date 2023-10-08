b2f83755900e7df9606ce2df5da8b360a467a57d
jennychiang <jennyc201911@gmail.com>
2023-10-08 20:11:43 +0800

    Update: Add a comment about timestamp to database migration script

--- db/migrate/20231005025036_create_todos.rb
+++ db/migrate/20231005025036_create_todos.rb
@@ -3,6 +3,7 @@ class CreateTodos < ActiveRecord::Migration[7.0]
     create_table :todos do |t|
       t.string :title
 
+      # 在這個表格分別建立出 created_at 以及 updated_at 兩個時間戳記欄位
       t.timestamps
     end
   end
